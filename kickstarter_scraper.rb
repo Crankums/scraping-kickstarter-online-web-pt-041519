d059466b77be6ccb4d6197caa77f80068d0f656b
Craig Donavin <crdonavin@gmail.com>
2019-05-30 02:17:54 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,6 +10,11 @@ require 'pry'
   def create_project_hash
     html = File.read("fixtures/kickstarter.html")
     kickstarter = Nokogiri::HTML(html)
+
     projects = {}
 
+    kickstarter.css("li.project.grid_4").each do |project|
+      projects[projects]= {}
+    end  
+    projects
   end  
